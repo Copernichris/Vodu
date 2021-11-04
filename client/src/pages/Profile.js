76d500c78a9b5493b3e501e0c4fa6bb0ddf7ec0c
Nickhyman465 <nickhyman465@gmail.com>
2021-11-03 21:35:57 -0400

    inital change to profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -50,14 +50,6 @@ const Profile = () => {
             showUsername={false}
           />
         </div>
-        {!userParam && (
-          <div
-            className="col-12 col-md-10 mb-3 p-3"
-            style={{ border: '1px dotted #1a1a1a' }}
-          >
-            <ThoughtForm />
-          </div>
-        )}
       </div>
     </div>
   );
