186229cea3b40edb0a0e32c7ac8d840e255286e2
Pamela <hammondpamelac@gmail.com>
2021-11-10 17:09:09 -0500

    added new fonts, changed positions of footer and profile page, styled search bar in header

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { Redirect, useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
+import React from "react";
+import { Redirect, useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
 
-import ThoughtForm from '../components/ThoughtForm';
-import ThoughtList from '../components/ThoughtList';
+import ThoughtForm from "../components/ThoughtForm";
+import ThoughtList from "../components/ThoughtList";
 
-import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import { QUERY_USER, QUERY_ME } from "../utils/queries";
 
-import Auth from '../utils/auth';
+import Auth from "../utils/auth";
 
 const Profile = () => {
   const { username: userParam } = useParams();
@@ -28,38 +28,43 @@ const Profile = () => {
 
   if (!user?.username) {
     return (
-      <h4>
-        You need to be logged in to see this. Use the navigation links above to
-        sign up or log in!
-      </h4>
+      <main className="flex-row justify-center align-center">
+        <div className="col-8">
+          <div className="card">
+            <h4 className="text-center p-3 mb-2 mt-2">
+              You need to be logged in to see this. Use the navigation links
+              above to login or signup!
+            </h4>
+          </div>
+        </div>
+      </main>
     );
   }
 
   return (
-    <div>
-      <div className="flex-row justify-center mb-3">
-        <h2 className="col-12 col-md-10 bg-dark text-light p-3 mb-5">
-          Viewing {userParam ? `${user.username}'s` : 'your'} profile.
-        </h2>
+    <main className="flex-row justify-center align-center">
+      <div className="col-md-8">
+        <div className="card">
+          <h2 className="card-header bg-black text-center text-white p-3 mb-2">
+            {Auth.getProfile().data.username}'s profile
+          </h2>
 
-        <div className="col-12 col-md-10 mb-5">
-          <ThoughtList
-            thoughts={user.thoughts}
-            title={`${user.username}'s thoughts...`}
-            showTitle={false}
-            showUsername={false}
-          />
-        </div>
-        {!userParam && (
-          <div
-            className="col-12 col-md-10 mb-3 p-3"
-            style={{ border: '1px dotted #1a1a1a' }}
-          >
-            <ThoughtForm />
+          <div className="col-12 col-md-10">
+            <ThoughtList
+              thoughts={user.thoughts}
+              title={`${user.username}'s thoughts...`}
+              showTitle={false}
+              showUsername={false}
+            />
           </div>
-        )}
+          {!userParam && (
+            <div className="col-12 col-md-10 mb-3 ">
+              <ThoughtForm />
+            </div>
+          )}
+        </div>
       </div>
-    </div>
+    </main>
   );
 };
 
