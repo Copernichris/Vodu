3e2aff49da8214c48d8dcc5e27a44b83ca41a554
Pamela <hammondpamelac@gmail.com>
2021-11-13 11:19:54 -0500

    reworded login or signup message and rerouted the login and signup pages to the modal buttons

--- client/src/components/CommentForm/index.js
+++ client/src/components/CommentForm/index.js
@@ -1,13 +1,15 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useMutation } from '@apollo/client';
+import React, { useState } from "react";
+import Login from "../../pages/Login";
+import Signup from "../../pages/Signup";
+import { Link } from "react-router-dom";
+import { useMutation } from "@apollo/client";
 
-import { ADD_COMMENT } from '../../utils/mutations';
+import { ADD_COMMENT } from "../../utils/mutations";
 
-import Auth from '../../utils/auth';
+import Auth from "../../utils/auth";
 
 const CommentForm = ({ vodId }) => {
-  const [commentText, setCommentText] = useState('');
+  const [commentText, setCommentText] = useState("");
   const [characterCount, setCharacterCount] = useState(0);
 
   const [addComment, { error }] = useMutation(ADD_COMMENT);
@@ -24,7 +26,7 @@ const CommentForm = ({ vodId }) => {
         },
       });
 
-      setCommentText('');
+      setCommentText("");
     } catch (err) {
       console.error(err);
     }
@@ -33,7 +35,7 @@ const CommentForm = ({ vodId }) => {
   const handleChange = (event) => {
     const { name, value } = event.target;
 
-    if (name === 'commentText' && value.length <= 280) {
+    if (name === "commentText" && value.length <= 280) {
       setCommentText(value);
       setCharacterCount(value.length);
     }
@@ -47,7 +49,7 @@ const CommentForm = ({ vodId }) => {
         <>
           <p
             className={`m-0 ${
-              characterCount === 280 || error ? 'text-danger' : ''
+              characterCount === 280 || error ? "text-danger" : ""
             }`}
           >
             Character Count: {characterCount}/280
@@ -63,7 +65,7 @@ const CommentForm = ({ vodId }) => {
                 placeholder="Add your comment..."
                 value={commentText}
                 className="form-input w-100"
-                style={{ lineHeight: '1.5', resize: 'vertical' }}
+                style={{ lineHeight: "1.5", resize: "vertical" }}
                 onChange={handleChange}
               ></textarea>
             </div>
@@ -76,9 +78,9 @@ const CommentForm = ({ vodId }) => {
           </form>
         </>
       ) : (
-        <p>
-          You need to be logged in to comment. Please{' '}
-          <Link to="/login">login</Link> or <Link to="/signup">signup.</Link>
+        <p className="comment-login text-white">
+          You need to be logged in to comment. <Login />
+          or <Signup />
         </p>
       )}
     </div>
--- client/src/pages/SingleVod.js
+++ client/src/pages/SingleVod.js
@@ -1,25 +1,24 @@
-import React from 'react';
+import React from "react";
 
 // Import the `useParams()` hook
-import { useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
+import { useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
 
-import CommentList from '../components/CommentList';
-import CommentForm from '../components/CommentForm';
+import CommentList from "../components/CommentList";
+import CommentForm from "../components/CommentForm";
 
-import VideoPage from '../components/VideoPage';
+import VideoPage from "../components/VideoPage";
 
-import { QUERY_SINGLE_VOD } from '../utils/queries';
+import { QUERY_SINGLE_VOD } from "../utils/queries";
 
 const SingleVod = () => {
   // Use `useParams()` to retrieve value of the route parameter `:profileId`
   const { vodId } = useParams();
-  
+
   const { loading, data } = useQuery(QUERY_SINGLE_VOD, {
     // pass URL parameter
     variables: { vodId: vodId },
   });
-  
 
   const vod = data?.vod || {};
 
@@ -27,11 +26,9 @@ const SingleVod = () => {
     return <div>Loading...</div>;
   }
   return (
-    <div>
-      <div style={{ height: '90vh', margin: 0, padding: 0 }}>
-        <VideoPage
-        comments={vod.comments}
-        video={vod} />            
+    <main>
+      <div style={{ height: "90vh", margin: 0, padding: 0 }}>
+        <VideoPage comments={vod.comments} video={vod} />
       </div>
       <div>
         <CommentList comments={vod.comments} />
@@ -39,7 +36,7 @@ const SingleVod = () => {
       <div>
         <CommentForm vodId={vod._id} />
       </div>
-    </div>
+    </main>
   );
 };
 
