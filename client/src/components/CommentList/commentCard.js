276e7d18de00ad1068c08e6241fb982e00a8c2ff
Nickhyman465 <nickhyman465@gmail.com>
2021-11-15 19:27:54 -0500

    baseline

--- client/src/components/CommentList/commentCard.js
+++ client/src/components/CommentList/commentCard.js
@@ -5,36 +5,33 @@ import { ADD_VOTES } from '../../utils/mutations';
 
 const CommentCard = ({ comment }) => {
     const [count, setCount] = useState(0);
-    const [addVotes, { error }] = useMutation(ADD_VOTES);
+    // const [addVotes, { error }] = useMutation(ADD_VOTES);
 
-    useEffect(() => { }, [count])
+    // useEffect(() => { }, [count])
     
-    const updateCount = async () => {
-        setCount(count + 1)
-        await addVotes({
-            variables: { 
-                vodId: comment._id,
-                voteCount: count,
-            }
-        })
-    }
+    // const updateCount = async () => {
+    //     setCount(count + 1)
+    //     await addVotes({
+    //         variables: { 
+    //             vodId: comment._id,
+    //             voteCount: count,
+    //         }
+    //     })
+    // }
     
     return (
         <div key={comment._id} className="flex-row col-12 mb-3 pb-3">
             {/* Adding upvote system. Still in progress. Need to add counter function*/}
             <div className="pt-2">
                 {/* Use onClick={} for function */}
-                <button className="upvote mb-0"
-                    name="upVote"
-                    value={count}
-                    onClick={() => updateCount()}>
+                <button className="upvote mb-0" onClick={() => setCount(count + 1)}>
                 ^
                 </button>
-                <h5 className="mb-0 pb-1 pt-1 text-white">
+                <h5 className="mb-2 pb-1 pt-1 text-white">
                 {count}
                 </h5>
                 <button className="downvote" onClick={() => setCount(count - 1)}>
-                v
+                V
                 </button>
             </div>
             {/* End of Upvote changes */}
