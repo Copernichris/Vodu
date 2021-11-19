935b5e828820023b34bd8a9b916cc1ec0005f406
copernichris <christhomas2101@gmail.com>
2021-11-19 14:56:16 -0500

    comment card and form fix

--- client/src/components/CommentList/index.js
+++ client/src/components/CommentList/index.js
@@ -6,20 +6,20 @@ const CommentList = ({ comments = [] }) => {
   if (!comments.length) {
     return (
       <div style={{ backgroundColor: "#1a1a1a" }}>
-        <h3 className="text-white">No Comments Yet</h3>
+        <h3 style={{paddingLeft: '20px', color: 'white'}}>No Comments Yet</h3>
       </div>
     );
   }
 
   return (
-    <>
+    <div>
       <h3 style={{paddingLeft: '20px', color: 'white'}}>Comments:</h3>
       {/* <div className="flex-row my-4"> */}
       <Stack style={{display: 'flex', direction: 'row'}}>
         {comments &&
           comments.map((comment) => <CommentCard comment={comment} />)}
       </Stack>
-    </>
+    </div>
   );
 };
 
